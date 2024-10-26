d7d3cb7a199804c9dc00885dbb258fee64c5eff2
afnan-rh96 <rkabbo13@gmail.com>
2024-10-26 19:40:35 +0600

    test

---
+++ java.js
@@ -0,0 +1,54 @@
+onload = () => {
+
+    const c = setTimeout(() => {
+
+      document.body.classList.remove("not-loaded");
+
+      clearTimeout(c);
+
+    }, 1000);
+
+  };//const
+const textInput = document.getElementById("textInput");
+const output = document.getElementById("output");
+
+//function to multiply vowels
+function longText(text, n) {
+  const vowels = ["a", "e", "i", "o", "u"];
+  var textArray = text.split("");
+  for (letter in textArray) {
+    const currentLetter = textArray[letter];
+    for (vowel in vowels) {
+      if (currentLetter == vowels[vowel]) {
+        textArray[letter] = textArray[letter].repeat(n);
+      }
+    }
+  }
+
+  output.innerHTML = textArray.join("");
+}
+
+textInput.addEventListener("input", function (e) {
+  longText(textInput.value, 2);
+});
+
+let love = [
+  {
+    msg: "hi!"
+  },
+  {
+    msg: "Keep moving Foward!"
+  },
+  {
+    msg: "Open your mind for new things"
+  },
+  {
+    msg: "You are more smart than you think"
+  }
+];
+
+let gerarPalavrasBonitas = function () {
+  const place = document.querySelector("span");
+  let n = Math.floor(Math.random() * love.length);
+  place.innerHTML = love[n].msg;
+};
